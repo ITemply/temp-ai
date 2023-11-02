fdff2dbf2ad0e12b9145f4d7bf001394fa4cd9ca
ITemply <120138779+ITemply@users.noreply.github.com>
2023-11-02 09:43:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def index():
   return 'helo'
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=2345)
+def startApp():
+  if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2345)
