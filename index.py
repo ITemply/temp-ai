cbe92af6219e1745fb5890e0003b693e6d44b3a1
ITemply <120138779+ITemply@users.noreply.github.com>
2023-11-03 09:51:52 -0400

    Update index.py

--- index.py
+++ index.py
@@ -8,6 +8,5 @@ app = Flask(__name__)
 def index():
   return 'helo'
 
-def startApp():
-  if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
