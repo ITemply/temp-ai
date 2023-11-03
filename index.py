e120f09d965f6e4f9d3b31821281dd8d6a342de6
ITemply <120138779+ITemply@users.noreply.github.com>
2023-11-03 10:41:44 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,10 @@
-import os, flask
-
 from flask import Flask, request, render_template, redirect, abort
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-  return 'index'
+  return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0')
