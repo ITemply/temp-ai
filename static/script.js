060e0c75bccf131de1d4640642f85f5071456465
ITemply <120138779+ITemply@users.noreply.github.com>
2023-11-03 21:43:10 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,16 @@
 console.log('hello world')
+
+async function log(datatype, collectdata) {
+  const sendinglogdata = {name: 'jay'}
+
+  try {
+    const senddata = await fetch('/log', {
+      method: 'POST',
+      body: JSON.stringify(sendinglogdata),
+      cache: 'default'
+    })
+    alert(senddata)
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
