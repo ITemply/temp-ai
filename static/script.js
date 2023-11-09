fd476c80df3e6e282d05a2ee1ae838adc49a5f07
I Temply <bluerambles278@gmail.com>
2023-11-09 19:04:10 +0000

    Added Redirect
    Signed-off-by: I Temply <bluerambles278@gmail.com>

--- static/script.js
+++ static/script.js
@@ -1,3 +1,18 @@
+function checkLogin() {
+  const username = localStorage.getItem('checkUsername')
+  const password = localStorage.getItem('checkPassword')
+
+  const location = window.location.href
+
+  if (username && password) {
+    if (location.includes('/signup') || location.includes('/signin')) {
+      window.location.href = '/home'
+    }
+  }
+}
+
+checkLogin()
+
 function logError(errorText) {
   let errorElement = document.getElementById('error')
   errorElement.innerHTML = errorText
