d4fa8c89b3019a03453741e63811239a7c39be48
I Temply <bluerambles278@gmail.com>
2023-11-20 13:52:04 +0000

    Added Render
    Signed-off-by: I Temply <bluerambles278@gmail.com>

--- index.py
+++ index.py
@@ -375,4 +375,4 @@ def userLeaving(clientDisconnectingData):
 # Flask
 
 if __name__ == '__main__':
-  socketio.run(app, host='0.0.0.0', port=3000)
+  socketio.run(app, host='0.0.0.0', port=3000, allow_unsafe_werkzeug=True)
